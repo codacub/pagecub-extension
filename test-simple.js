779a01fc4ed0f496c63f65f1ca597df7f589f634
Elliot Roberts <elliot.roberts@gmail.com>
2026-01-25 20:55:43 +0000

    File tidy up

--- test-simple.js
+++
@@ -1,6 +0,0 @@
-console.log('🟢 TEST: Simple file executing');
-window.SIMPLE_TEST = "working";
-window.ThreadCubTagging = function() { 
-  console.log('🟢 TEST: ThreadCubTagging called'); 
-};
-console.log('🟢 TEST: ThreadCubTagging defined as:', typeof window.ThreadCubTagging);
